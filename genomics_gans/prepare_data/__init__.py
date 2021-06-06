ab36bd3bea4c85ac72a34f336079a68c66dcf993
Unique Divine <u.divine@columbia.edu>
2021-06-05 19:58:52 -0500

    tests: test_root, test_prepare_data.

---
+++ genomics_gans/prepare_data/__init__.py
@@ -0,0 +1,24 @@
+import os, sys
+
+def access_root_dir(depth = 1):
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    if depth >= 1:
+        assert isinstance(depth, int)
+        args: list = [parent_dir]
+        for _ in range(depth):
+            args.append('..')
+        rel_path = os.path.join(*args)
+    elif depth == 0:
+        rel_path = parent_dir
+    else:
+        raise ValueError("Invalid depth")
+
+    sys.path.append(rel_path) 
+
+access_root_dir(depth=0)
+
+import genomics_gans
+import warnings
+warnings.filterwarnings("ignore")
+
---
+++ genomics_gans/tests/test_imports.py
@@ -0,0 +1,19 @@
+import os, sys
+
+class TestImports:
+
+    def test_root(self):
+        current_file = os.path.realpath(__file__)
+        current_file_parent_dir = os.path.dirname(current_file)
+        root_dir = os.path.dirname(current_file_parent_dir)
+        os.chdir(root_dir)
+        exec(open('__init__.py').read()) 
+    
+    def test_prepare_data_dir(self):
+        current_file = os.path.realpath(__file__)
+        current_file_parent_dir = os.path.dirname(current_file)
+        root_dir = os.path.dirname(current_file_parent_dir)
+        os.chdir(root_dir)
+        os.chdir("prepare_data")
+        exec(open('__init__.py').read()) 
+    
